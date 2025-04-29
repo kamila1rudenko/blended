709d4ec2cbd6ffa1cecb7ea50df55d7afc6ad33c
Kamila <kamillkaa95@gmail.com>
2025-04-29 23:05:02 +0300

    i

--- js/lesson_4.js
+++ js/lesson_4.js
@@ -22,3 +22,53 @@ const chngButton = document.getElementById('chngButton');
 chngButton.addEventListener('click', () => {
     paragraphs.forEach((paragraph, index) => { paragraph.textContent = index + 1; });
 });
+
+
+const passwordInput = document.getElementById('passwordInput');
+const passwordButton = document.getElementById('passwordButton');
+
+passwordButton.addEventListener('click', () => {
+  if (passwordInput.type === 'text') {
+    passwordInput.type = 'password';
+    passwordButton.textContent = 'Розкрити';
+  } else {
+    passwordInput.type = 'text';
+    passwordButton.textContent = 'Приховати';
+  }
+});
+
+
+const box = document.getElementById('box');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+
+increaseBtn.addEventListener('click', () => {
+  const currentWidth = box.offsetWidth;
+  const currentHeight = box.offsetHeight;
+  box.style.width = currentWidth + 10 + 'px';
+  box.style.height = currentHeight + 10 + 'px';
+});
+
+decreaseBtn.addEventListener('click', () => {
+  const currentWidth = box.offsetWidth;
+  const currentHeight = box.offsetHeight;
+  
+  if (currentWidth > 10 && currentHeight > 10) {
+    box.style.width = currentWidth - 10 + 'px';
+    box.style.height = currentHeight - 10 + 'px';
+  }
+});
+
+
+//TODO: № 7 ==============================================
+// Натиснувши кнопку "Подвоювати", збільшити значення
+// у кожному елементі списку у 2 рази
+
+
+//TODO: № 9 ==============================================
+// Напишіть інтерфейс, щоб створити список
+// Для кожного пункту:
+// Запитуйте вміст у користувача за допомогою promt
+// створювати пункт та додавати його до списку
+// процес припиняється коли користувач натискає ESC
+// усі елементи мають створюватись динамічно
